dac7b4c4d1bd169f4aabbc33fc675a04099554bd
Ken Katagiri <kkatagiri3@gatech.edu>
2016-10-26 22:11:24 -0400

    Ghosts chase pacman using a_star. Algo uses Goog closure library.

--- directives/ghost-agent.js
+++ directives/ghost-agent.js
@@ -16,7 +16,10 @@ pacmanApp.directive("ghostAgent", function($document, $interval) {
         scope.name = attrs.name;
 
         function moveable(gridY, gridX) {
-            return (scope.GRID[gridY*GRID_HEIGHT+gridX] == 0) || (scope.GRID[gridY*GRID_HEIGHT+gridX] == 1);
+            if ((gridY < 0) || (gridY >= GRID_HEIGHT) || (gridX < 0) || (gridX >= GRID_WIDTH)) {
+                return false;
+            }
+            return (scope.GRID[gridY*GRID_WIDTH+gridX] == 0) || (scope.GRID[gridY*GRID_WIDTH+gridX] == 1);
         }
         
         scope.$on("reset", function() {
@@ -41,40 +44,58 @@ pacmanApp.directive("ghostAgent", function($document, $interval) {
             }
         }
         /* Chase Pacman */
-        /*function chasingDirection() {
+        function chasingDirection() {
             /* Perform a depth-limited a_star search towards pacman */
-            /*var pGridY = Math.floor(state.pacmanLocation[0]/CELL_HEIGHT);
-            var pGridX = Math.floor(state.pacmanLocation[1]/CELL_WIDTH);
-            var gridY = Math.floor(state.y/CELL_HEIGHT);
-            var gridX = Math.floor(state.x/CELL_WIDTH);
+            var pGridY = Math.floor(scope.pacmanLocation[0]/CELL_HEIGHT);
+            var pGridX = Math.floor(scope.pacmanLocation[1]/CELL_WIDTH);
+            var pIndex = pGridY*GRID_WIDTH + pGridX;
+            var gridY = Math.floor(scope.y/CELL_HEIGHT);
+            var gridX = Math.floor(scope.x/CELL_WIDTH);
             /* The Manhattan distance heuristic */
-            /*function heuristic(y, x) {
-                return Math.abs(searchGridY-pGridY) + Math.abs(searchGirdX-pGridX);
+            function heuristic(gridY, gridX) {
+                return Math.abs(gridY-pGridY) + Math.abs(gridX-pGridX);
             }
-            /*function a_star(pGridY, pGridX, gridY, gridX) {
-                
+            /* The return type is a STRING representative of the direction. Caller must take this and change to appropriate number... */
+            function a_star(pGridY, pGridX, gridY, gridX) {
                 var frontier = new goog.structs.Heap()
-                frontier.insert(0, {endCoord : [gridY, gridX]
-                                         firstDirection: null})
+                frontier.insert(heuristic(gridY, gridX),
+                                        {lastIndex : gridY*GRID_WIDTH+gridX,
+                                          distSoFar : 0,
+                                          initialDirection: null});
                 var explored = new goog.structs.Set;
+                var vals, lastIndex, distSoFar, initialDirection, possibleDirections, index;
                 while (!frontier.isEmpty()) {
-                    frontier.pop
-                }
-                
-                
-                
-                /* key: distance, value: considered paths of said distance - can be multiple paths per distance */
-                /*var frontier = {0: [[[gridY, gridX]]]}
-                var explored = {};
-                var keys = [0];
-                while keys.length != 0:
-                    var dist = keys[0]
-                    var path = paths[dist].pop()
-                    var lastCoord = path[path.length-1]
-                    for ()*/
+                    vals = frontier.remove();
+                    lastIndex = vals["lastIndex"];
+                    distSoFar = vals["distSoFar"];
+                    initialDirection = vals["initialDirection"];
                     
-            
-        //}
+                    explored.add(lastIndex);
+                    /* Possible Directions to go from lastIndex, and which index it'll take me to. Keys will always be strings in JS... */
+                    possibleDirections = {
+                        "LEFT" : lastIndex-1,
+                        "RIGHT" : lastIndex+1,
+                        "UP" : lastIndex-GRID_WIDTH,
+                        "DOWN" : lastIndex+GRID_WIDTH
+                    };
+                    for (var direction in possibleDirections) {
+                        index = possibleDirections[direction];
+                        if (!explored.contains(index) && moveable(Math.floor(index/GRID_WIDTH), index%GRID_WIDTH)) {
+                            /* If index is where pacman is, return initialDirection needed to get there. */
+                            if (index === pIndex) {
+                                return initialDirection==null ? direction : initialDirection;
+                            }
+                            frontier.insert(distSoFar+1+heuristic(Math.floor(index/GRID_WIDTH), index%GRID_WIDTH),
+                                                    {lastIndex : index,
+                                                      distSoFar : distSoFar+1,
+                                                      initialDirection: initialDirection==null ? direction : initialDirection});
+                        }
+                    }
+                }
+                return "RIGHT";
+            }
+            scope.intendedDirection = scope[a_star(pGridY, pGridX, gridY, gridX)];
+        }
         
         /* Scatter */
         
@@ -138,7 +159,7 @@ pacmanApp.directive("ghostAgent", function($document, $interval) {
         }
         var timeoutId = $interval(timeAction, TIME_INTERVAL);
         var statechangeTimeoutId = $interval(changeState, 20*TIME_INTERVAL*Math.max(Math.random(), 0.5));
-        var rdTimeoutId = $interval(randomDirectionChange, 15*TIME_INTERVAL*Math.max(Math.random(), 0.5));
+        var rdTimeoutId = $interval(chasingDirection, 15*TIME_INTERVAL*Math.max(Math.random(), 0.5));
         element.on("$destroy", function() {
             $interval.cancel(timeoutId);
             $interval.cancel(rdTimeoutId);
--- pacman.js
+++ pacman.js
@@ -1,4 +1,10 @@
 
+/* Require closure library heap and set for a_star. */
+goog.require('goog.iter');
+goog.require('goog.structs');
+goog.require('goog.structs.Heap');
+goog.require('goog.structs.Set');
+
 var pacmanApp = angular.module("pacman", []);
 
 var GRID_HEIGHT = 20;
@@ -11,3 +17,11 @@ var NUM_MOUTH_STATES = 5;
 var NUM_GHOST_STATES = 2;
 var SPEED = 5;
 var TIME_INTERVAL = 50;
+
+
+/* DO NOT CHANGE. VALUES ARE USED FOR KEY-BINDINGS, AMONG OTHERS */
+var LEFT = 37;
+var UP = 38;
+var RIGHT = 39;
+var DOWN = 40;
+
